d24caa420c0979f56a1fb49b13f26f6d25e1f09e
Jeroen <jegbol01@gmail.com>
2021-05-30 16:20:26 +0200

    Added collision detection

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-window.onload = () => {
+let rig;
 
+window.onload = () => {
+    const collisionTests = document.getElementsByClassName('collisionTest')
+    rig = document.getElementById('rig')
+
+    for (const collisionTest of collisionTests) {
+        collisionTest.addEventListener('hitstart', playerHit)
+    }
+}
+
+const playerHit = (e) => {
+    console.log('Player has been hit');
 }
