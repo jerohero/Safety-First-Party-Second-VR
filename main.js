11334715fa5619211d92386e8bbc94e60561ffa4
Jeroen <jegbol01@gmail.com>
2021-08-04 12:48:40 +0200

    Added function that generates array of random positions

--- main.js
+++ main.js
@@ -7,8 +7,43 @@ window.onload = () => {
     for (const collisionTest of collisionTests) {
         collisionTest.addEventListener('hitstart', playerHit)
     }
+
+    randomEnemies()
 }
 
 const playerHit = (e) => {
     console.log('Player has been hit');
+}
+
+const randomEnemies = () => {
+    var enemies = [],
+        enemy = {},
+        overlapping = false,
+        maxEnemies = 200,
+        protection = 10000,
+        counter = 0
+
+    while (enemies.length < maxEnemies && counter < protection) {
+        enemy = {
+            x: Math.floor(Math.random() * 101),
+            z: Math.floor(Math.random() * 101),
+            r: 10
+        }
+        overlapping = false;
+
+        for (var i = 0; i < enemies.length; i++) {
+            var existing = enemies[i];
+            var d = Math.sqrt((enemy.x - existing.x) ** 2 + (enemy.y - existing.y) ** 2);
+            if (d < enemy.r + existing.r) {
+            overlapping = true;
+            break;
+            }
+        }
+        
+        if (!overlapping) {
+            enemies.push(enemy);      
+        }
+        
+        counter++;
+    }
 }
