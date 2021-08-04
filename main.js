a1c45368dd4492d79f2f9cc717f4d2d22b7ec192
Jeroen <jegbol01@gmail.com>
2021-08-04 14:17:00 +0200

    Renamed playerhit

--- main.js
+++ main.js
@@ -1,18 +1,9 @@
 let rig;
 
 window.onload = () => {
-    const collisionTests = document.getElementsByClassName('collisionTest')
     rig = document.getElementById('rig')
 
-    for (const collisionTest of collisionTests) {
-        collisionTest.addEventListener('hitstart', playerHit)
-    }
-
     randomEnemies()
-}
-
-const playerHit = (e) => {
-    console.log('Player has been hit')
 }
 
 const randomEnemies = () => {
