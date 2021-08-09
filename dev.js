af1606f697c3884180ab064fd905e26474659a65
Jeroen <jegbol01@gmail.com>
2021-08-09 12:39:13 +0200

    RandomEnemies to dev.js

--- dev.js
+++ dev.js
@@ -3,10 +3,13 @@ AFRAME.registerComponent('dev', {
         initDev()
     },
 });
+
 const initDev = () => {
     let camera = document.getElementById("camera")
     let rig = document.getElementById("rig")
     let cursor = document.getElementById("cursor")
+
+    randomEnemies()
 
     const collisionTests = document.getElementsByClassName('collisionTest')
     for (const collisionTest of collisionTests) {
@@ -16,4 +19,44 @@ const initDev = () => {
 
 const hitPlayer = (e) => {
     console.log('Player has been hit')
+}
+
+const randomEnemies = () => {
+    const enemies = generateEnemiesList()
+    console.log(enemies);
+}
+
+const generateEnemiesList = () => {
+    var enemies = [],
+        enemy = {},
+        overlapping = false,
+        maxEnemies = 200,
+        protection = 10000,
+        counter = 0
+
+    while (enemies.length < maxEnemies && counter < protection) {
+        enemy = {
+            x: Math.floor(Math.random() * 101),
+            z: Math.floor(Math.random() * 101),
+            r: 10
+        }
+        overlapping = false
+
+        for (var i = 0; i < enemies.length; i++) {
+            var existing = enemies[i]
+            var d = Math.sqrt((enemy.x - existing.x) ** 2 + (enemy.y - existing.y) ** 2)
+            if (d < enemy.r + existing.r) {
+                overlapping = true;
+                break;
+            }
+        }
+        
+        if (!overlapping) {
+            enemies.push(enemy)
+        }
+        
+        counter++
+    }
+
+    return enemies
 }
--- main.js
+++ main.js
@@ -9,10 +9,8 @@ window.onload = () => {
         festival: document.getElementById('dev-scene')
     }
 
-    toIntroScene()
-    // toFestivalScene()
-
-    randomEnemies()
+    // toIntroScene()
+    toFestivalScene()
 }
 
 const toIntroScene = () => {
@@ -24,37 +22,4 @@ const toFestivalScene = () => {
     scenes.room.removeAttribute('room')
     scenes.room.setAttribute('visible', 'false')
     scenes.festival.setAttribute('visible', 'true')
-}
-
-const randomEnemies = () => {
-    var enemies = [],
-        enemy = {},
-        overlapping = false,
-        maxEnemies = 200,
-        protection = 10000,
-        counter = 0
-
-    while (enemies.length < maxEnemies && counter < protection) {
-        enemy = {
-            x: Math.floor(Math.random() * 101),
-            z: Math.floor(Math.random() * 101),
-            r: 10
-        }
-        overlapping = false
-
-        for (var i = 0; i < enemies.length; i++) {
-            var existing = enemies[i]
-            var d = Math.sqrt((enemy.x - existing.x) ** 2 + (enemy.y - existing.y) ** 2)
-            if (d < enemy.r + existing.r) {
-                overlapping = true;
-                break;
-            }
-        }
-        
-        if (!overlapping) {
-            enemies.push(enemy)
-        }
-        
-        counter++
-    }
 }
